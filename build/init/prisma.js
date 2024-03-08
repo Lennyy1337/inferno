2758efd47441f28babca1ac99d7565c8fdce85bf
2g4g6 <2g4@riseup.net>
2024-03-08 15:32:24 +0100

    remove build

--- build/init/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-const prisma_field_encryption_1 = require("prisma-field-encryption");
-const client = new client_1.PrismaClient();
-exports.prisma = client.$extends((0, prisma_field_encryption_1.fieldEncryptionExtension)());
