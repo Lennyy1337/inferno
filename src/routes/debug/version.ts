9240bbdf4ba4966a604a5ef22fd0b1b6a70f3645
2g4g6 <2g4@riseup.net>
2024-03-09 17:09:10 +0100

    trol

--- src/routes/debug/version.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-async function routes(fastify: FastifyInstance, options: any) {
-    fastify.get('/version', (request, reply) => {
-        reply.send({version: "v1"})
-    });
-}
-
-export default routes;
